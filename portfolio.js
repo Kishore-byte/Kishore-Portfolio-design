e5df03ae1dcf324a72e9a87c8a1f9a698fef6666
kizto <ashokkishore1411@gmail.com>
2024-06-29 11:32:38 +0530

    portfolio design

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+//popup program
+
+var shadow=document.querySelector(".black-shadow")
+var box1=document.querySelector(".popup-box1")
+var close=document.querySelector(".close")
+
+function fun()
+{
+    shadow.style.display="block"
+    box1.style.display="block"
+}
+function closed()
+{
+    shadow.style.display="none"
+    box1.style.display="none"
+}
